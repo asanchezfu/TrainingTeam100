f794d17ff54306e39b4f9b8a2e5c5d9bb2db963c
Juan Diego Ramirez Lemos <jramirezle@unal.edu.co>
2024-01-31 18:43:54 -0500

    Fix server.js

--- Music Store/server.js
+++ Music Store/server.js
@@ -1,31 +1,31 @@
-const { MongoClient, ObjectId } = require('mongodb');
+// Importa el módulo de Express
+const express = require('express');
+const database = require('./connectionExpress');
 
-let client;
-const connect = async () => {
-  try {
-    //const uri = 'mongodb://localhost:27017';
-    const uri = 'mongodb+srv://jramirezle:Milo.2020@cluster0.aecvodh.mongodb.net/?retryWrites=true&w=majority';
-    client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // ... otras opciones de configuración si es necesario
-    });
+// Crea una instancia de la aplicación Express
+const app = express();
 
-    await client.connect();
-    console.log('Conexión exitosa a la base de datos');
-    _db = client.db("Music_Store"); // Reemplaza "tuNombreDeDB" con el nombre real de tu base de datos
-    
-  } catch (error) {
-    console.error('Error de conexión a la base de datos:', error.message);
-  }
-};
 
-var _db;
+//MId
+app.use(express.json());
 
-module.exports = {
-   connect,
-   getDb: function () {
-    return _db;
-  },
-  ObjectId: ObjectId, // Asegúrate de exportar ObjectId desde aquí
-};
+//Importar Ruta
+const myFirst = require('./routes/myFirstRoute');
+
+//Implementar ruta
+app.use('/', myFirst);
+
+// Connect to mongodb database
+database.connect();
+
+
+// Puerto en el que escuchará el servidor
+const PORT = 3000;
+
+// Inicia el servidor
+const server = app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
+
+module.exports = { server };
+
